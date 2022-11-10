b470cc0b2dbbf1db16d774c662ef1dd07a292937
Serhii Mazur <serhii.mazur@beetroot.se>
2022-11-10 23:12:23 +0200

    ADD | env/sh/git

---
+++ .env-core/sh/projects/git/git_actions.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+set -o errexit #to stop the script when an error occurs
+set -o pipefail
+
+git_actions() {
+    while true; do
+        EMPTY_LINE
+        ECHO_INFO "========= GIT actions ========="
+        ECHO_YELLOW "0 - Return to the previous menu"
+        ECHO_GREEN "1 - Clone from repo"
+        ECHO_GREEN "2 - Create Github repo"
+        ECHO_GREEN "3 - Create Gitlab repo"
+        ECHO_GREEN "4 - Repo access"
+        read -rp "$(ECHO_YELLOW "Please select one of:")" actions
+
+        case $actions in
+        0)
+            actions_existing_project
+            ;;
+        1)
+            git_clone_repo
+            unset_variables
+            actions_existing_project
+            ;;
+        2)
+            get_existing_domains "====== Create Github Repo ====="
+            get_project_dir "skip_question"
+            git_create_repo_github
+            unset_variables
+            git_actions
+            ;;
+        3)
+            get_existing_domains "====== Create GitLab Repo ====="
+            get_project_dir "skip_question"
+            git_create_repo_gitlab
+            unset_variables
+            git_actions
+            ;;
+        4)
+            git_save_access
+            git_actions
+            ;;
+        esac
+    done
+}
