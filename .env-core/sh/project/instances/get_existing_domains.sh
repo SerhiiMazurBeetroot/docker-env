f076245aadd6f6d36abbedd5ba87aac4e042e3b9
Serhii Mazur <serhii.mazur@beetroot.se>
2024-12-26 18:09:29 +0200

    UPDATE | fn get_existing_domains by project status (active|inactive)

--- .env-core/sh/project/instances/get_existing_domains.sh
+++ .env-core/sh/project/instances/get_existing_domains.sh
@@ -7,11 +7,34 @@ get_existing_domains() {
     ACTION=$1
 
     if [ -z "$DOMAIN_NAME" ]; then
+        EMPTY_LINE
+        ECHO_CYAN "======== Project Status ======="
+        ECHO_YELLOW "[0] Return to the services menu"
+        ECHO_KEY_VALUE "[1]" "active [default]"
+        ECHO_KEY_VALUE "[2]" "inactive"
+        ECHO_KEY_VALUE "[3]" "all"
+
+        first_choice=$(GET_USER_INPUT "select_one_of")
+
+        if [[ "$first_choice" == "0" ]]; then
+            project_services_menu
+        fi
+
+        filter=""
+        case "$first_choice" in
+        1) filter="^active\|" ;;            # starting with "active|"
+        2) filter="^inactive\|" ;;          # starting with "inactive|"
+        3) filter="^(active|inactive)\|" ;; # both "active|" and "inactive|"
+        *)
+            filter="^active\|"
+            ;;
+        esac
+
         string=$(awk '{print $3 $4 $5}' "$FILE_INSTANCES" | tail -n +2)
 
         if [ "$string" ]; then
             #Check project status is active
-            string="$(echo "${string}" | grep -oE '(active|inactive)\|[A-Za-z0-9.-]*')"
+            string="$(echo "${string}" | grep -E "$filter")"
             OptionList=($string)
 
             while true; do
@@ -32,7 +55,7 @@ get_existing_domains() {
                     break
                 else
                     if [ "$choice" == 0 ]; then
-                        project_services_menu
+                        docker_menu
                     else
                         ECHO_WARN_RED "Wrong option"
                     fi
