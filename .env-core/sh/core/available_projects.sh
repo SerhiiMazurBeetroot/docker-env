d8fb76a05f3b6c99b455ada6400d14976160a95c
Serhii Mazur <mazyr.s.n@gmail.com>
2026-01-29 21:03:32 +0200

    UPDATE | macOS | Avoid arrays with export, initialize all variables

--- .env-core/sh/core/available_projects.sh
+++ .env-core/sh/core/available_projects.sh
@@ -1,23 +1,25 @@
 #!/bin/bash
 
 # BASH_VERSION 3.2.57 (default macOS)
-export PROJECT_TITLES=(
-    "Wordpress"
-    "BEDROCK"
-    "PHP-Server"
-    "Next.js"
-    "Directus"
-    "Elastic Stack"
-    "Directus + Next.js"
-    "Next.WP"
-    "Node.js"
+PROJECT_TITLES=(
+	"Wordpress"
+	"BEDROCK"
+	"PHP-Server"
+	"Next.js"
+	"Directus"
+	"Elastic Stack"
+	"Laravel"
+	"Directus + Next.js"
+	"Next.WP"
+	"Node.js"
 )
 
-export AVAILABLE_PROJECTS=(
-    "wordpress"
-    "bedrock"
-    "php"
-    "nextjs"
-    "directus"
-    "elasticsearch"
+AVAILABLE_PROJECTS=(
+	"wordpress"
+	"bedrock"
+	"php"
+	"nextjs"
+	"directus"
+	"elasticsearch"
+	"laravel"
 )
